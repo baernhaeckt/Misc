3429170261ab15ec017ca569a468cd2685f03b5b
Marc Sallin <marc.sallin@outlook.com>
2019-08-24 17:53:46 +0200

    Set new user for GetBearer

--- Scripts/GetBearer.ps1
+++ Scripts/GetBearer.ps1
@@ -1,5 +1,5 @@
 
-$answer = Invoke-WebRequest -Uri "http://localhost:5000/api/users/Login?email=m%40m.ch&password=1234" -Method "POST"
+$answer = Invoke-WebRequest -Uri "http://localhost:5000/api/users/Login?email=marc.sallin@outlook.com&password=1234" -Method "POST"
 $parsedAnswer = $answer.Content | ConvertFrom-Json
 $parsedAnswer.token
 $t = $parsedAnswer.token
